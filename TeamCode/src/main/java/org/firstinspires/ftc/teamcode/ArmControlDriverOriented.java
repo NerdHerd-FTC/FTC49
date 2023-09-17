ef799a5d912e0d3e9cf05ac5140d7c6571f861a5
Derrick K <derrick.kwan.360@gmail.com>
2023-09-17 10:58:02 -0700

    Update ArmControlDriverOriented.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControlDriverOriented.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControlDriverOriented.java
@@ -14,20 +14,12 @@ public class ArmControlDriverOriented extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare motors (F=front, B=back, R=right, L=left)
         DcMotor motorFL = hardwareMap.dcMotor.get("frontLeft");
-        DcMotor motorBL = hardwareMap.dcMotor.get("backLeft");
-        DcMotor motorFR = hardwareMap.dcMotor.get("frontRight");
-        DcMotor motorBR = hardwareMap.dcMotor.get("backRight");
-        DcMotor motorTB = hardwareMap.dcMotor.get("topBottom");
 
 
-        // Right motors should move in reverse
-        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
-        // Left motors should move forward
-        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
-        // Top Bottom Motor move forward
-        motorTB.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // Right motors should move forward
+        motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
+
         // Retrieve the IMU from the hardware map
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -44,26 +36,18 @@ public class ArmControlDriverOriented extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Y stick is reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double theta = gamepad1.right_stick_x; //Rotate by theta
-            double right_trigger = gamepad1.right_trigger; // Up
-            double left_trigger = gamepad2.left_trigger; // Down
+     
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(theta), 1);
             //Set motor powers
-            double FLPower = (y + x + theta) / denominator;
-            double BLPower = (y - x + theta) / denominator;
-            double FRPower = (y - x - theta) / denominator;
-            double BRPower = (y + x - theta) / denominator;
-            double TBPower = (right_trigger + left_trigger) / denominator;
+            double FLPower = (y + x - theta) / denominator;
 
 
             //Run motors using powers
             motorFL.setPower(FLPower);
-            motorBL.setPower(BLPower);
-            motorFR.setPower(FRPower);
-            motorBR.setPower(BRPower);
-            motorTB.setPower(TBPower);
+
         }
     }
 }
