2fe7ba71f90493f96b93dc872cabe8561321fce1
Derrick K <derrick.kwan.360@gmail.com>
2023-09-18 10:25:57 -0700

    Update ArmControlDriverOriented.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControlDriverOriented.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControlDriverOriented.java
@@ -17,38 +17,29 @@ public class ArmControlDriverOriented extends LinearOpMode {
 
 
 
-        // Right motors should move forward
+        // Right motors should move in reverse
         motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        // Retrieve the IMU from the hardware map
-        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        // Technically this is the default, however specifying it is clearer
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        // Without this, data retrieving from the IMU throws an exception
-        imu.initialize(parameters);
-
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Y stick is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double theta = gamepad1.right_stick_x; //Rotate by theta
-     
+            double y = -gamepad1.left_stick_x; // Y stick is reversed
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+
+
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(theta), 1);
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) , 1);
             //Set motor powers
-            double FLPower = (y + x - theta) / denominator;
+            double FLPower = ( y + x ) / denominator;
+
 
 
             //Run motors using powers
             motorFL.setPower(FLPower);
-
+            telemetry.addData("Arm Encoder", motorFL.getCurrentPosition());
         }
     }
 }
-
